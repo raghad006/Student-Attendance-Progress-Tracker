c529f567d49d22d8d110a6dfb75eb4ccae65cbab
raghad006 <raghadalmaghraby11@gmail.com>
2025-12-04 20:37:49 +0200

    Updated Database to MySQL

--- attendance_project/attendance_project/settings.py
+++ attendance_project/attendance_project/settings.py
@@ -85,12 +85,19 @@ WSGI_APPLICATION = 'attendance_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
+
 
 
 # Password validation
